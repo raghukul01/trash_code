434cddcaa3fb4ce50ebcc3283a75eb9689484b82
Raghukul Raman <raghukul.raman01@gmail.com>
2019-01-29 22:34:20 +0530

    Add some operators

--- t1.go
+++ t1.go
@@ -9,8 +9,35 @@ func main() {
 	b := 1.2
 	c := 1 + 2.12i
 	d := "this is so random!"
-	var e char;
-	e = 'x'
+	e := 'x'
+
+	a = a + b
+	a = a - b
+	a = a * b
+	a = a / b
+
+
+	m := make(map[int]int)
+	m[1] = 2
+	m[2] = 4
+
+	m[3] = m[2] % m[1]
+	m[3] += m[2]
+	m[3] -= m[2]
+	m[3] *= m[2]
+	m[3] /= m[2]
+	m[3] %= m[2]
+	m[3] << 1
+	var ck1, ck2 bool
+	ck1 = true
+	ck2 = false
+
+	ck1 = ck2 && ck1
+	ck2 = ck1 || ck2
+	ck2 |= ck1
+
+	ck1 &= ck2
+
 	fmt.Println(a, b, c)
 	fmt.Println(d, e)
 }
